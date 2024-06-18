f569c7e46fc89554fb43fa6082213a4f218c9ba7
AYU-o <71383946+AYU-o@users.noreply.github.com>
2024-06-19 00:54:52 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,21 @@
+x=1
+for i in range(1,6):
+  p=str(i)+" "
+  print(p*x)
+  x=x+1
+
+print("\n")
+
 x=5
 for i in range(1,6):
   p=str(i)+" "
   print(p*x)
   x=x-1
 
+print("\n")
+
+x=1
+for i in range(1,6):
+  p=str("x")+" "
+  print(p*x)
+  x=x+1  
